398db684d237c88ef6eec5619423300e0c093898
seanw7 <seanw@protonmail.ch>
2017-03-18 14:56:51 -0700

    First commit of c 2 f conversion program

---
+++ celsius2fah.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 14 16:19:48 2017
+
+@author: SW
+"""
+
+
+
+def c_to_f(c):
+    fah= c *9/5+32
+    if fah < -273.15:
+        print ("Below Absolute Zero: Impossible temp")
+    print (fah)
+
+def calcTemp():
+    while True:
+        inputTemp = int(input("Please input temperature in Celsius for conversion: "))
+        c_to_f(inputTemp)
+        
+
+calcTemp()
+
+
+
