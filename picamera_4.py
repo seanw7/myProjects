c3b20776f3122fe20759f88534c1899004c57574
seanw7 <seanw@protonmail.ch>
2017-05-01 08:18:13 -0700

    added an empty space between timestamp and count of photo

--- picamera_4.py
+++ picamera_4.py
@@ -15,7 +15,7 @@ def normalCam():
 	camera.resolution = (1920,1080)
         sleep(2)
         Photoname = strftime("%Y_%m_%d_%H_%M_%s")
-        camera.capture('/home/pi/Pictures/{}_{}.jpg'.format(Photoname, count))
+        camera.capture('/home/pi/Pictures/{} {}.jpg'.format(Photoname, count))
         print('NormalFilter: Captured {}'.format(Photoname))
         sleep(10)# wait 5 minutes
 
@@ -36,7 +36,7 @@ def lowLightCam():
         # to mode switching on the still port, this will take
         # longer than 6 seconds
         Photoname = strftime("%Y_%m_%d_%H_%M_%s")
-        camera.capture('/home/pi/Pictures/{}_{}.jpg'.format(Photoname, count))
+        camera.capture('/home/pi/Pictures/{} {}.jpg'.format(Photoname, count))
         print("LowLightFilter: Captured {}".format(count))
 
 def hourCheck():
